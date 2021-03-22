6f9738583ebe5f54112f13677dd8ab1df6315bb7
Martin Pompéry <ms@funkpopes.org>
2021-03-22 19:23:09 +0100

    Version 0.5.7

--- dist/types/types.d.ts
+++ dist/types/types.d.ts
@@ -4,7 +4,7 @@
  */
 export declare type SessionId = string;
 export declare type FunctionId = string;
-export declare type ComputationKind = "RANKING" | "FUNCTION_CALL";
+export declare type ComputationKind = "RANKING" | "RANKING_DATASET" | "RANKING_DATASET_DELEGATED" | "FUNCTION_CALL" | "FUNCTION_CALL_DELEGATED";
 /** minimal type for error reponses from an API */
 export interface ErrorApiResponse {
     success: false;
--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,12 @@ export type SessionId = string;
 
 export type FunctionId = string;
 
-export type ComputationKind = "RANKING" | "FUNCTION_CALL";
+export type ComputationKind =
+  | "RANKING"
+  | "RANKING_DATASET"
+  | "RANKING_DATASET_DELEGATED"
+  | "FUNCTION_CALL"
+  | "FUNCTION_CALL_DELEGATED";
 
 /** minimal type for error reponses from an API */
 export interface ErrorApiResponse {
